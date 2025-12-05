182f36c5353e1a98f6874a6fad39659b7c80c734
Geovane Una <geovanetcampos73@gmail.com>
2025-12-04 21:25:11 -0300

    alterando o arquivo yaml

--- js/testes.js
+++ js/testes.js
@@ -3,6 +3,21 @@ function assert(condicao, mensagem) {
             throw new Error(mensagem);
         }
 }
+function somar(a, b) {
+    return a + b;
+}
+function subtrair(a, b) {
+    return a - b;
+}
+function multiplicar(a, b) {
+    return a * b;
+}
+function dividir(a, b) {
+if (b === 0)
+    throw new Error("Divisão por zero não permitida");
+    return a / b;
+}
+
 function rodarTestes() {
     console.log("Iniciando testes...");
     // Testes para somar
